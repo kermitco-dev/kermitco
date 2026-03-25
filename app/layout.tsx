16a6079d58898f39bd69c70604760c28758714ba
kermitco-dev <masonwklee@gmail.com>
2026-03-24 23:14:09 -0400

    Added Navbar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/app/nav";
 
 const geistSans = Geist({ // i have a feeling this font is good :smooch:
   variable: "--font-geist-sans",
@@ -22,12 +23,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+        >
+            
+            <body className="min-h-full flex flex-col bg-slate-600"><Navbar />{children}</body>
+        </html>
+    );
 }
---
+++ app/nav.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React, { ReactNode } from "react";
+
+
+interface Props {
+    children?: ReactNode
+    href?: string
+    height?: number
+    width?: number
+    src?: string
+}
+
+export function Navbar() {
+    return (
+        <nav className="bg-black fixed w-full z-20 top-0 inset-s-0 border-b border-gray-800">
+            <div className="flex flex-wrap items-center justify-between mx-auto p-1 pr-2">
+                <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <Icon src="/icon_full.png" height={35}/>
+                    <span className="self-center text-xl text-heading font-semibold whitespace-nowrap font-google-sans">kermitco v6</span>
+                </Link>
+                <div className="w-full md:block md:w-auto">
+                    <ul className="font-medium flex flex-col border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-3 md:mt-0 md:border-0 md:bg-neutral-primary">
+                        <NavElement href="/">Home</NavElement>
+                        <NavElement href="/news/">News</NavElement>
+                        <NavElement href="/games/">Games</NavElement>
+                        <NavElement href="/settings/">Settings</NavElement>
+                        <NavElement href="https://github.com/kermitco-dev/kermitco/">
+                            <Icon height={32} src="/github_ico.png"/>
+                        </NavElement>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export function Icon({src="/icon_full.png", height}: Props) {
+    return (
+        <Image src={src} height={height} width={height} alt="kermitco icon."/>
+    )
+}
+
+function NavElement({href, children}: Props) {
+    return (
+        <li className=" p-3">
+            <Link href={{href}} className="block text-white bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0 font-google-sans" aria-current="page">{children}</Link>
+        </li>
+    )
+}
