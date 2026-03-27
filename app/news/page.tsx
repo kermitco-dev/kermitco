1a0a998c77e9b0d8386f47be2128cf2b11181a99
kermitco-dev <masonwklee@gmail.com>
2026-03-26 21:03:40 -0400

    Added news/page.tsx.

---
+++ app/news/page.tsx
@@ -0,0 +1,16 @@
+import {MainContentBox} from "@/utils/KermitcoSpecific"
+
+export default function News() {
+    return (
+        <MainContentBox>
+            <p>
+                <span className="font-bold font-google-sans text-white tracking-wide text-3xl">kermitco v6</span>
+                <br/>
+                <span className="text-sm">News</span>
+                <br/>
+                <br/>
+                TODO: add news lmao
+            </p>
+        </MainContentBox>
+    )
+}
--- utils/KermitcoSpecific.tsx
+++ utils/KermitcoSpecific.tsx
@@ -8,7 +8,7 @@ export function MainContentBox({children}:Props) {
     return (
         <main className="mt-14 h-full">
             <div className="flex flex-col flex-1 items-center justify-center font-sans p-4 h-max">
-                <div className="flex flex-auto max-w-5xl min-w-xl mx-auto bg-gray-950 rounded-md border border-gray-800 p-6 justify-center">
+                <div className="flex flex-auto max-w-5xl min-w-xl mx-auto bg-gray-950 rounded-md border border-gray-800 p-6 justify-center text-center">
                     {children}
                 </div>
             </div>
